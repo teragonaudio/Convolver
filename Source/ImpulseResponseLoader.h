f614e6d5640b7797c57b1ca028b6a2f5eb3ad717
Nik Reiman <nik.reiman@gmail.com>
2013-03-24 12:52:20 +0100

    Add Impluse Response loader to project

---
+++ Source/ImpulseResponseLoader.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ImpulseResponseLoader.h
+    Created: 24 Mar 2013 12:45:13pm
+    Author:  nik
+
+  ==============================================================================
+*/
+
+#ifndef __IMPULSERESPONSELOADER_H_8B889137__
+#define __IMPULSERESPONSELOADER_H_8B889137__
+
+
+
+
+
+#endif  // __IMPULSERESPONSELOADER_H_8B889137__
