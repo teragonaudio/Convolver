43aa483cbcb151d7da9b83885293ed9fa0b77c28
Nik Reiman <nik.reiman@gmail.com>
2013-03-24 14:15:53 +0100

    Add FFTWrapper to project

--- Source/FFTWrapper.cpp
+++ Source/FFTWrapper.cpp
@@ -9,3 +9,30 @@
 */
 
 #include "FFTWrapper.h"
+
+FFTWrapper::FFTWrapper(int bufferSize)
+{
+#if USE_FFT_FIXED_SIZE
+// TODO: Find best fit for size
+#else
+    fftProcessor = new ffft::FFTReal<float>(bufferSize);
+#endif
+}
+
+FFTWrapper::~FFTWrapper()
+{
+    if (fftProcessor) {
+        delete fftProcessor;
+    }
+}
+
+void FFTWrapper::doFFT(const float* inputData, float* outputData) const
+{
+    fftProcessor->do_fft(outputData, inputData);
+}
+
+void FFTWrapper::doIFFT(const float* inputData, float* outputData) const
+{
+    fftProcessor->do_ifft(inputData, outputData);
+    fftProcessor->rescale(outputData);
+}
--- Source/FFTWrapper.h
+++ Source/FFTWrapper.h
@@ -11,8 +11,27 @@
 #ifndef __FFTWRAPPER_H_2DA48785__
 #define __FFTWRAPPER_H_2DA48785__
 
+#define USE_FFT_FIXED_SIZE 0
+#if USE_FFT_FIXED_SIZE
+#include "FFTRealFixLen.h"
+#else
+#include "FFTReal.h"
+#endif
 
+class FFTWrapper {
+public:
+    FFTWrapper(int bufferSize);
+    virtual ~FFTWrapper();
 
+    void doFFT(const float* inputData, float* outputData) const;
+    void doIFFT(const float* inputData, float* outputData) const;
 
+private:
+#if USE_FFT_FIXED_SIZE
+    // TODO: Allocate several fixed-length processors, pick the best at runtime
+#else
+    ffft::FFTReal<float> *fftProcessor;
+#endif
+};
 
 #endif  // __FFTWRAPPER_H_2DA48785__
